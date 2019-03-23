e2fba8c2fa1c27128bd6d873799d4337a00efabe
sabrinamusatian <sabrina.musatian@yandex.ru>
2019-03-23 20:44:57 +0300

    add positioning

--- app/src/main/java/com/example/kirill/mtcfit/MapFragmentView.java
+++ app/src/main/java/com/example/kirill/mtcfit/MapFragmentView.java
@@ -14,14 +14,56 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
+import com.here.android.mpa.common.PositioningManager;
+import com.here.android.mpa.common.LocationDataSourceHERE;
+import com.here.android.positioning.StatusListener;
+
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.GeoPosition;
+import com.here.android.mpa.common.LocationDataSourceHERE;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.common.PositioningManager;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.SupportMapFragment;
+import com.here.android.mpa.mapping.MapState;
+import com.here.android.positioning.StatusListener;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 //import java.util.Map;
 
-public class MapFragmentView {
+public class MapFragmentView implements PositioningManager.OnPositionChangedListener{
     private SupportMapFragment m_mapFragment;
     private AppCompatActivity m_activity;
     private Map m_map;
+    private PositioningManager mPositioningManager;
+    private LocationDataSourceHERE mHereLocation;
+    // flag that indicates whether maps is being transformed
+    private boolean mTransforming;
+    // callback that is called when transforming ends
+    private Runnable mPendingUpdate;
+
+
+    @Override
+    public void onPositionUpdated(final PositioningManager.LocationMethod locationMethod, final GeoPosition geoPosition, final boolean mapMatched) {
+        final GeoCoordinate coordinate = geoPosition.getCoordinate();
+        if (mTransforming) {
+            mPendingUpdate = new Runnable() {
+                @Override
+                public void run() {
+                    onPositionUpdated(locationMethod, geoPosition, mapMatched);
+                }
+            };
+        } else {
+            m_map.setCenter(coordinate, Map.Animation.BOW);
+        }
+    }
+
+    @Override
+    public void onPositionFixChanged(PositioningManager.LocationMethod locationMethod, PositioningManager.LocationStatus locationStatus) {
+        // ignored
+    }
+
 
     public MapFragmentView(AppCompatActivity activity) {
         m_activity = activity;
@@ -78,7 +120,71 @@ public class MapFragmentView {
                              * Please refer to the user guide for how to get the real-time location.
                              */
 
-                            m_map.setCenter(new GeoCoordinate(49.258576, -123.008268), Map.Animation.NONE);
+//                            m_map.setCenter(new GeoCoordinate(49.258576, -123.008268), Map.Animation.NONE);
+                            m_map.setZoomLevel(m_map.getMaxZoomLevel() - 1);
+//                            m_map.addTransformListener(m_activity);
+                            mPositioningManager = PositioningManager.getInstance();
+                            m_map.setCenter(mPositioningManager.getPosition().getCoordinate(), Map.Animation.NONE);
+                            Log.e(String.valueOf(mPositioningManager.getPosition().getCoordinate().getLongitude()), String.valueOf(mPositioningManager.getPosition().getCoordinate().getLatitude()));
+                            mHereLocation = LocationDataSourceHERE.getInstance(
+                                    new StatusListener() {
+                                        @Override
+                                        public void onOfflineModeChanged(boolean offline) {
+                                            // called when offline mode changes
+                                        }
+
+                                        @Override
+                                        public void onAirplaneModeEnabled() {
+                                            // called when airplane mode is enabled
+                                        }
+
+                                        @Override
+                                        public void onWifiScansDisabled() {
+                                            // called when Wi-Fi scans are disabled
+                                        }
+
+                                        @Override
+                                        public void onBluetoothDisabled() {
+                                            // called when Bluetooth is disabled
+                                        }
+
+                                        @Override
+                                        public void onCellDisabled() {
+                                            // called when Cell radios are switch off
+                                        }
+
+                                        @Override
+                                        public void onGnssLocationDisabled() {
+                                            // called when GPS positioning is disabled
+                                        }
+
+                                        @Override
+                                        public void onNetworkLocationDisabled() {
+                                            // called when network positioning is disabled
+                                        }
+
+                                        @Override
+                                        public void onServiceError(ServiceError serviceError) {
+                                            // called on HERE service error
+                                        }
+
+                                        @Override
+                                        public void onPositioningError(PositioningError positioningError) {
+                                            // called when positioning fails
+                                        }
+
+                                        @Override
+                                        public void onWifiIndoorPositioningNotAvailable() {
+                                            // called when running on Android 9.0 (Pie) or newer
+                                        }
+                                    });
+                            mPositioningManager.setDataSource(mHereLocation);
+                            mPositioningManager.addListener(new WeakReference<PositioningManager.OnPositionChangedListener>(
+                                    MapFragmentView.this));
+                            // start position updates, accepting GPS, network or indoor positions
+                            if (mPositioningManager.start(PositioningManager.LocationMethod.GPS_NETWORK_INDOOR)) {
+                                m_mapFragment.getPositionIndicator().setVisible(true);
+                            }
                         } else {
                             Toast.makeText(m_activity,
                                     "ERROR: Cannot initialize Map with error " + error,
