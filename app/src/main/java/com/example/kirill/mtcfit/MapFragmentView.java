b655a1c1eec97a916424809c87233ba1cb1ebe0a
sabrinamusatian <sabrina.musatian@yandex.ru>
2019-03-23 19:28:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kirill/mtcfit/MapFragmentView.java
@@ -0,0 +1,92 @@
+package com.example.kirill.mtcfit;
+
+import android.app.Activity;
+
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.SupportMapFragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+//import java.util.Map;
+
+public class MapFragmentView {
+    private SupportMapFragment m_mapFragment;
+    private AppCompatActivity m_activity;
+    private Map m_map;
+
+    public MapFragmentView(AppCompatActivity activity) {
+        m_activity = activity;
+        initMapFragment();
+    }
+
+    private SupportMapFragment getMapFragment() {
+        return (SupportMapFragment) m_activity.getSupportFragmentManager().findFragmentById(R.id.mapfragment);
+    }
+
+    private void initMapFragment() {
+        /* Locate the mapFragment UI element */
+        m_mapFragment = getMapFragment();
+
+        // Set path of isolated disk cache
+        String diskCacheRoot = Environment.getExternalStorageDirectory().getPath()
+                + File.separator + ".isolated-here-maps";
+        // Retrieve intent name from manifest
+        String intentName = "";
+        try {
+            ApplicationInfo ai = m_activity.getPackageManager().getApplicationInfo(m_activity.getPackageName(),
+                    PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            intentName = bundle.getString("INTENT_NAME");
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(this.getClass().toString(), "Failed to find intent name, NameNotFound: " + e.getMessage());
+        }
+
+        boolean success = com.here.android.mpa.common.MapSettings.setIsolatedDiskCacheRootPath(diskCacheRoot,
+                intentName);
+        if (!success) {
+            // Setting the isolated disk cache was not successful, please check if the path is valid and
+            // ensure that it does not match the default location
+            // (getExternalStorageDirectory()/.here-maps).
+            // Also, ensure the provided intent name does not match the default intent name.
+        } else {
+            if (m_mapFragment != null) {
+                /* Initialize the SupportMapFragment, results will be given via the called back. */
+                m_mapFragment.init(new OnEngineInitListener() {
+                    @Override
+                    public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
+
+                        if (error == Error.NONE) {
+                            /*
+                             * If no error returned from map fragment initialization, the map will be
+                             * rendered on screen at this moment.Further actions on map can be provided
+                             * by calling Map APIs.
+                             */
+                            m_map = m_mapFragment.getMap();
+
+                            /*
+                             * Map center can be set to a desired location at this point.
+                             * It also can be set to the current location ,which needs to be delivered by the PositioningManager.
+                             * Please refer to the user guide for how to get the real-time location.
+                             */
+
+                            m_map.setCenter(new GeoCoordinate(49.258576, -123.008268), Map.Animation.NONE);
+                        } else {
+                            Toast.makeText(m_activity,
+                                    "ERROR: Cannot initialize Map with error " + error,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
