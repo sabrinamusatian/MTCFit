da6853ae84d22718134cc341c27651ce000ced62
cmirnov <cmirnov239kirill@gmail.com>
2019-03-24 11:09:55 +0300

    rate

--- app/src/main/java/com/example/kirill/mtcfit/MainActivity.java
+++ app/src/main/java/com/example/kirill/mtcfit/MainActivity.java
@@ -1,19 +1,10 @@
 package com.example.kirill.mtcfit;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 /**
  * Main activity which launches map view and handles Android run-time requesting permission.
@@ -22,6 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
     ImageView start_map;
+    ImageView rate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +25,22 @@ public class MainActivity extends Activity {
                 start_map();
             }
         });
+        start_map = findViewById(R.id.rate);
+        start_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start_rate();
+            }
+        });
     }
 
     public void start_map() {
         Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    public void start_rate() {
+        Intent intent = new Intent(this, RateActivity.class);
         startActivity(intent);
     }
 }
